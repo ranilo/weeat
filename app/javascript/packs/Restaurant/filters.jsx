4ee3f84a2d5754bb9fe39123bc17a6de04961dfd
ranilo <ranilo@gmail.com>
2018-10-11 22:29:23 +0300

    combine filters (still

--- app/javascript/packs/Restaurant/filters.jsx
+++ app/javascript/packs/Restaurant/filters.jsx
@@ -5,19 +5,44 @@ class Filters extends React.Component {
     constructor(props) {
         super(props);
         this.handleNameChange = this.handleNameChange.bind(this);
-        this.state = {value: ''};
+        this.handleRatingChange = this.handleRatingChange.bind(this)
+        this.state = {name: '', rating: 0};
+    }
+    filterRating = (i)=>{return i.rating >= this.state.rating};
+    filterName = (i) => {return i.name.includes(this.state.name)};
+
+    combineFilters = () => {
+        console.log(this.state);
+        this.props.onFilterChange(i => (  this.filterRating(i)  &&  this.filterName(i)));
+    }
+
+    handleRatingChange(e) {
+        const rating = e.target.value;
+        this.setState({rating: rating});
+        console.log("update rating "+this.state.rating);
+
+        this.combineFilters();
     }
 
     handleNameChange(e) {
         const str = e.target.value;
-        this.setState({value: str});
-        this.props.onFilterChange(i => (i.name.includes( str)));
+        this.setState({name: str});
+        this.combineFilters();
     }
 
     render() {
         return <div className='filters'>
-            <input type='text' value={this.state.value}
-                   onChange={this.handleNameChange}/>
+            <div className='names'>
+                name
+                <input type='text' value={this.state.name}
+                       onChange={this.handleNameChange}/>
+            </div>
+            <div className='rating'>
+                rating
+                <input value={this.state.rating}
+                       onChange={this.handleRatingChange}/>
+            </div>
+
         </div>
     }
 
